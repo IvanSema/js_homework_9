a514ed43b0360c205dcd09d729493b73b601128e
IvanSema <ivan_8d@ukr.net>
2020-05-06 15:44:14 +0300

    correct version first homework

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ document.addEventListener('keydown', function(event) {
     document.getElementById('js-textarea').innerHTML = document.getElementById('text-block').innerHTML;
 
     if (event.code == 'KeyE' && event.ctrlKey) {
+      event.preventDefault()
       document.getElementById('text-block').style.display = 'none';
       document.getElementById('js-textarea').style.display = 'block'; 
     }
 
 
     if (event.code == 'Equal' && event.ctrlKey) {
+      event.preventDefault()
       document.getElementById('text-block').innerHTML = document.getElementById('js-textarea').value;
       document.getElementById('text-block').style.display = 'block';
       document.getElementById('js-textarea').style.display = 'none'; 
